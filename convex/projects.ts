de312dbbfc24ba45be9a6b303a53d26b5c8f88b1
kinimody <94915772+kinimody@users.noreply.github.com>
2026-02-02 22:42:23 +0100

    background jobs

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,7 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
-import { error } from "console";
-import { id } from "date-fns/locale";
+
 
 export const create = mutation({
   args: {
